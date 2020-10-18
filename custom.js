8f4cc7c10facdf47c865d72997a9fef7a504f047
DangKhoa <iunc129@gmail.com>
2020-10-19 02:48:06 +0700

    update

--- custom.js
+++ custom.js
@@ -3,10 +3,44 @@ document.querySelector('#btnTinhTien').onclick = function () {
     var tongTienThanhToan = document.querySelector('#txtTongTien').value;
     var phanTramTip = document.querySelector('#phanTramTip').value;
     var share = document.querySelector('#soNguoiShare').value;
+    console.log(tongTienThanhToan);
+    //Tính toán và in ra giao diện
 
-    //Tính toán
-    var tienTip = (Number(tongTienThanhToan * phanTramTip / 100) / share);
-
-    //In ra giao diện
-    document.querySelector('#tongTienTip').innerHTML = tienTip.toLocaleString() + '$';
+    if (tongTienThanhToan === "" && phanTramTip === "" && share === "") {
+        document.querySelector('#divTongTien').style.display = 'block';
+        document.querySelector('#divShare').style.display = 'block';
+        document.querySelector('#divPhanTramTip').style.display = 'block';
+    }
+    else if (tongTienThanhToan === "" && phanTramTip === "" && share != "") {
+        document.querySelector('#divPhanTramTip').style.display = 'block';
+        document.querySelector('#divTongTien').style.display = 'block';
+        document.querySelector('#divShare').style.display = 'none';
+    }
+    else if (tongTienThanhToan === "" && phanTramTip != "" && share != "") {
+        document.querySelector('#divTongTien').style.display = 'block';
+        document.querySelector('#divShare').style.display = 'none';
+        document.querySelector('#divPhanTramTip').style.display = 'none';
+    }
+    else if (tongTienThanhToan != "" && phanTramTip === "" && share === "") {
+        document.querySelector('#divPhanTramTip').style.display = 'block';
+        document.querySelector('#divTongTien').style.display = 'none';
+        document.querySelector('#divShare').style.display = 'block';
+    }
+    else if (tongTienThanhToan != "" && phanTramTip != "" && share === "") {
+        document.querySelector('#divShare').style.display = 'block';
+        document.querySelector('#divTongTien').style.display = 'none';
+        document.querySelector('#divPhanTramTip').style.display = 'none';
+    }
+    else if (tongTienThanhToan === "" && phanTramTip != "" && share === "") {
+        document.querySelector('#divPhanTramTip').style.display = 'none';
+        document.querySelector('#divTongTien').style.display = 'block';
+        document.querySelector('#divShare').style.display = 'block';
+    }
+    else if (tongTienThanhToan != "" && phanTramTip != "" && share != "") {
+        var tienTip = (Number(tongTienThanhToan * phanTramTip / 100) / share);
+        document.querySelector('#tongTienTip').innerHTML = tienTip.toLocaleString() + '$';
+        document.querySelector('#divTongTien').style.display = 'none';
+        document.querySelector('#divShare').style.display = 'none';
+        document.querySelector('#divPhanTramTip').style.display = 'none';
+    }
 }
