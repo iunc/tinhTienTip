5125fa0c009adfd95df9db2fc9af4815907e036b
DangKhoa <iunc129@gmail.com>
2020-10-19 13:59:16 +0700

    update

--- custom.js
+++ custom.js
@@ -3,9 +3,8 @@ document.querySelector('#btnTinhTien').onclick = function () {
     var tongTienThanhToan = document.querySelector('#txtTongTien').value;
     var phanTramTip = document.querySelector('#phanTramTip').value;
     var share = document.querySelector('#soNguoiShare').value;
-    console.log(tongTienThanhToan);
-    //Tính toán và in ra giao diện
 
+    //Tính toán và in ra giao diện
     if (tongTienThanhToan === "" && phanTramTip === "" && share === "") {
         document.querySelector('#divTongTien').style.display = 'block';
         document.querySelector('#divShare').style.display = 'block';
@@ -36,7 +35,12 @@ document.querySelector('#btnTinhTien').onclick = function () {
         document.querySelector('#divTongTien').style.display = 'block';
         document.querySelector('#divShare').style.display = 'block';
     }
-    else if (tongTienThanhToan != "" && phanTramTip != "" && share != "") {
+    else if (tongTienThanhToan != "" && phanTramTip === "" && share != "") {
+        document.querySelector('#divPhanTramTip').style.display = 'block';
+        document.querySelector('#divTongTien').style.display = 'none';
+        document.querySelector('#divShare').style.display = 'none';
+    }
+    else {
         var tienTip = (Number(tongTienThanhToan * phanTramTip / 100) / share);
         document.querySelector('#tongTienTip').innerHTML = tienTip.toLocaleString() + '$';
         document.querySelector('#divTongTien').style.display = 'none';
